73cc518dbe27801c5cd41e306320bbf2ba2d3236
Ali Hasan <capt.3lii@gmail.com>
2024-11-23 18:14:26 +0300

    test commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { SafeAreaView } from 'react-native';
 import React from 'react';
-import Home from './src/screens/HomeScreen/Home';
+import { SafeAreaView } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import AppStack from './src/stack/AppStack';
 
 const App = () => {
