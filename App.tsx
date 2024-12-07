e95aab56bef6f84a7c0cf06e9c4422701f94415f
Ali Hasan <capt.3lii@gmail.com>
2024-12-07 19:29:18 +0300

    update readme + add amplify

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 
+import { Authenticator } from '@aws-amplify/ui-react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { AmplifyConfiguration } from './amplify/config';
 import AppStack from './src/stack/AppStack';
+
+AmplifyConfiguration();
 
 const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
-        <AppStack />
+        <Authenticator.Provider>
+          <Authenticator>
+            <AppStack />
+          </Authenticator>
+        </Authenticator.Provider>
       </SafeAreaView>
     </NavigationContainer>
   );
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Uber Clone
+
+## Add Signin/Signup using Amplify AWS
+
+1. Run the below command to create amplify file in your project
+
+```
+npm create amplify@latest
+```
+
+2. Remove the data file: amplify/data
+3. Add configuration file inside amplify folder
